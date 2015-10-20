1d3f7664f954308d807ade8f7044a1911a9c0397
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-10-20 14:38:14 +0300

    table php start

--- table.php
+++ table.php
@@ -1,1 +1,10 @@
-table
+<?php
+	require_once("functions.php");
+	
+	if(isset($_GET["delete"])) {
+		deletePostData($_GET["delete"]);
+	}
+	
+	if(isset($_GET["edit"])){
+		editPostData($_GET["car_id"], $_GET["mileage"], $_GET["cost"], $_GET["description"]);
+	}
