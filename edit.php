627a54af8f25fc23cf670c36c6231972094feb5d
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-10-20 16:23:47 +0300

    edit php algus

--- edit.php
+++ edit.php
@@ -1,1 +1,8 @@
-edit.php
+<?php
+    require_once("edit_functions.php");
+     
+    //Kasutaja muudab andmeid
+    if(isset($_GET["update"])){
+        //auto id, auto mudel, auto läbisõit, kulu, kirjeldus
+        updateCarData($_GET["car_id"], $_GET["carmodel"], $_GET["mileage"], $_GET["cost], $_GET["description"]);
+    }
