ce6504b596cd3b7e1c7dcd5a634f8e12c7e25e22
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-10-20 15:15:54 +0300

    functions.php started

--- functions.php
+++ functions.php
@@ -1,1 +1,21 @@
-functions
+<?php
+
+
+	require_once("../../config_global.php");
+	$database = "if15_karl";
+	
+	function getAllData($keyword=""){
+		
+		if($keyword == ""){
+			$search = "%%";
+		}else{
+			$search = "%".$keyword."%";
+		}
+		
+		
+	}
+	
+	
+	
+	
+?>
