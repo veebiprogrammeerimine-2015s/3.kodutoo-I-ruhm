f2e2a6c261625a53f8ce1673484e4b10e01b08b3
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-10-20 15:22:46 +0300

    functions.php building?

--- functions.php
+++ functions.php
@@ -12,7 +12,12 @@
 			$search = "%".$keyword."%";
 		}
 		
-		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+        // deleted IS NULL - ei ole kustutatud
+        $stmt = $mysqli->prepare("SELECT id, user_id, mileage, cost, description FROM car_costs WHERE deleted IS NULL AND (mileage LIKE ? OR description like ? OR cost LIKE ?)");
+        $stmt->bind_param("ss", $search, $search);
+        $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
+        $stmt->execute();
 	}
 	
 	
