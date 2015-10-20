345cc696ae93a581616a2919c722644037254a5f
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-10-20 15:39:00 +0300

    added table

--- table.php
+++ table.php
@@ -8,3 +8,31 @@
 	if(isset($_GET["edit"])){
 		editPostData($_GET["car"], $_GET["mileage"], $_GET["cost"], $_GET["description"]);
 	}
+	
+	$keyword = "";
+	if(isset($_GET["keyword"])){
+		$keyword = $_GET["keyword"];
+		
+		$car_array = getAllData($keyword);
+		
+	}else{
+		$car_array = getAllData();
+	}
+	?>
+	
+	<h1>Tabel</h1>
+	<form action="table.php" method="get">
+		<input name="keyword" type="search" value="<?=$keyword?>">
+		<input type="submit" value="otsi">
+	<form>
+	<br><br>
+	<table border=1>
+	<tr>
+	<th>id</th>
+    <th>kasutaja id</th>
+    <th>auto</th>
+    <th>läbisõit</th>
+    <th>kulu</th>
+    <th>kirjeldus</th>
+    <th></th>
+	</tr>
