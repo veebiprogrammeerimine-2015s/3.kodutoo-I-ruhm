cf92ab99e15c2463271b64a2d51d780cbf77465f
Karl Grossberg <karlgro@greeny.cs.tlu.ee>
2015-10-20 15:47:40 +0300

    näitame/ei näita

--- table.php
+++ table.php
@@ -41,7 +41,20 @@
 	for($i = 0; $i < count($car_array); $i++)
 		
 	if(isset($_GET["edit"]) && $_GET["edit"] == $car_array[$i]->id){
-		
+		echo "<tr>";
+            echo "<form action='table.php' method='get'>";
+            // input mida välja ei näidata
+            echo "<input type='hidden' name='car_id' value='".$car_array[$i]->id."'>";
+            echo "<td>".$car_array[$i]->id."</td>";
+            echo "<td>".$car_array[$i]->user_id."</td>";
+            echo "<td><input name='car' value='".$car_array[$i]->car."' ></td>";
+            echo "<td><input name='mileage' value='".$car_array[$i]->mileage."' ></td>";
+			echo "<td><input name='cost' value='".$car_array[$i]->cost."' ></td>";
+			echo "<td><input name='description' value='".$car_array[$i]->description."' ></td>";
+            echo "<td><input name='update' type='submit'></td>";
+            echo "<td><a href='table.php'>cancel</a></td>";
+            echo "</form>";
+            echo "</tr>";
 		
 	}
 ?>
