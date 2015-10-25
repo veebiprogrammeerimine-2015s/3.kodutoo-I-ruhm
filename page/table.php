a0600a579a0c03d1095778b0c1d5d5ed2af058c8
Katariina Linde <klinde@greeny.cs.tlu.ee>
2015-10-25 23:43:20 +0200

    added search

--- page/table.php
+++ page/table.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once("tabel_fun.php");
+    require_once("table_functions.php");
     
     //kuulan, kas kasutaja tahab kustutada
     if(isset($_GET["delete"])){
@@ -9,25 +9,40 @@
     
     //kasutaja muudab andmeid
     if(isset($_GET["update"])){
-        updateContestData($_GET["contest_id"], $_GET["contest_name"], $_GET["name"]);
+        updateCarData($_GET["contest_id"], $_GET["contest_number"], $_GET["name"]);
     }
     
     //kõik autod objektide kujul massiivis
     $contest_array = getAllData();
+    
+    $keyword = "";
+    if(isset($_GET["keyword"])){
+        $keyword = $_GET["keyword"];
+        //otsime
+        $contest_array = getAllData($keyword);
+    }else{
+        //näitame kõik tulemused
+        $contest_array = getAllData();
+    }
 ?>
 
 <h1>Tabel</h1>
+<form action="table.php" method="get">
+    <input name="keyword" type="search" value="<?=$keyword?>">
+    <input type="submit" value="Otsi"><br><br>
+</form>
 <table border=1>
 <tr>
     <th>id</th>
     <th>Kasutaja id</th>
-    <th>Võistluse nimi</th>
+    <th>Võistlus</th>
     <th>Osaleja nimi/klubi</th>
     <th>Kustuta</th>
     <th>Muuda</th>
+    <th></th>
 </tr>
 <?php
-    //autod ükshaaval läbi käia
+    //osalejad ükshaaval läbi käia
     for($i = 0; $i < count($contest_array); $i++){
         
         //kasutaja tahab rida muuta
@@ -53,6 +68,7 @@
             echo "<td>".$contest_array[$i]->name."</td>";
             echo "<td><a href='?delete=".$contest_array[$i]->id."'>X</a></td>";
             echo "<td><a href='?edit=".$contest_array[$i]->id."'>Muuda</a></td>";
+            echo "<td><a href='edit.php?edit_id=".$contest_array[$i]->id."'>Muuda eraldi lehel</a></td>";
             echo "</tr>";
             
         }
