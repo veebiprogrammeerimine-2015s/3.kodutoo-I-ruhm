1226a9cf47992b235cbd86ab7b86bccb355404f7
Richard Aasa <richard.aasa@gmail.com>
2016-01-29 17:32:54 +0200

    Added To-do into data.php.

--- data.php
+++ data.php
@@ -4,7 +4,7 @@
     $database = "if15_richaas_1";
 
     $mysqli = new mysqli($servername, $username, $password, $database);
-    
+
     // Kas kasutaja on sisse loginud?
     if(!isset($_SESSION['logged_in_user_id'])){
         header("Location: login.php");
@@ -17,6 +17,18 @@
         session_destroy();
         header("Location: login.php");
     }
+
+    $todo = $date = '';
+    $todo_error = $date_error = '';
 ?>
 
 Tere, <?=$_SESSION['logged_in_user_email'];?> <a href="?logout=1">Logi välja</a>
+
+<h2>Todo:</h2>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+    <label for="todo">To do content:</label>
+   	<input id="todo" name="todo" type="text" value="<?=$todo;?>"> <?=$todo_error;?><br><br>
+   	<label for="date"> Esitamise kuupäev </label>
+    <input id="date" name="date" type="text" value="<?=$date;?>"> <?=$date_error;?><br><br>
+   	<input type="submit" name="add_todo" value="Lisa">
+  </form>
